334e3073d7dcffa44a6141598107a1e737b2d80c
max matejka <matej065@morris.umn.edu>
2026-01-30 17:37:39 -0600

    added a lot of tests to increase coverage.

--- server/src/main/java/umm3601/todo/TodoController.java
+++ server/src/main/java/umm3601/todo/TodoController.java
@@ -10,10 +10,7 @@ import java.security.NoSuchAlgorithmException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
-import java.util.Objects;
 import java.util.regex.Pattern;
-import com.mongodb.client.model.Filters;
-
 
 import org.bson.Document;
 import org.bson.UuidRepresentation;
@@ -39,7 +36,6 @@ public class TodoController implements Controller {
   static final String AGE_KEY = "age";
   static final String COMPANY_KEY = "company";
   static final String ROLE_KEY = "role";
-  static final String SORT_ORDER_KEY = "sortorder";
   private static final int REASONABLE_AGE_LIMIT = 150;
   private static final String ROLE_REGEX = "^(admin|editor|viewer)$";
   public static final String EMAIL_REGEX = "^[a-zA-Z0-9_!#$%&'*+/=?`{|}~^.-]+@[a-zA-Z0-9.-]+$";
@@ -48,87 +44,66 @@ public class TodoController implements Controller {
 
   public TodoController(MongoDatabase database) {
     todoCollection = JacksonMongoCollection.builder().build(
-      database,
-      "todos", Todo.class, UuidRepresentation.STANDARD);
+      database, "todos", Todo.class, UuidRepresentation.STANDARD);
   }
 
   public void getTodo(Context ctx) {
     String id = ctx.pathParam("id");
     Todo todo;
-
-     try {
+    try {
       todo = todoCollection.find(eq("_id", new ObjectId(id))).first();
-     } catch (IllegalArgumentException e) {
+    } catch (IllegalArgumentException e) {
       throw new BadRequestResponse("The requested todo id wasn't a legal Mongo Object ID.");
-     }
-     if (todo == null) {
-      throw new NotFoundResponse("The requested todo was not found");
-     } else {
-      ctx.json(todo);
-      ctx.status(HttpStatus.OK);
-     }
+    }
+    if (todo == null) throw new NotFoundResponse("The requested todo was not found");
+
+    ctx.json(todo);
+    ctx.status(HttpStatus.OK);
   }
 
   public void getTodos(Context ctx) {
-    String statusParam = ctx.queryParam("status");
+    Bson filter = constructFilter(ctx);
+    Bson sort = Sorts.ascending("name");
 
-    Bson combinedFilter = constructFilter(ctx);
-    Bson sortingOrder = Sorts.ascending("name");
-    if ("true".equals(statusParam)){
-      combinedFilter = combineFilters(combinedFilter, Filters.eq("completed", true));
-
-
-    }
     String limitParam = ctx.queryParam("limit");
     int limit = (limitParam != null) ? Integer.parseInt(limitParam) : 0;
-    ArrayList<Todo> matchingTodos = todoCollection
-      .find(combinedFilter)
-      .sort(sortingOrder)
+
+    List<Todo> todos = todoCollection
+      .find(filter)
+      .sort(sort)
       .limit(limit > 0 ? limit : 0)
       .into(new ArrayList<>());
 
-      ctx.json(matchingTodos);
+    ctx.json(todos);
     ctx.status(HttpStatus.OK);
-
   }
-
-  private Bson combineFilters(Bson originalFilter, Bson newFilter) {
-    return Filters.and(originalFilter, newFilter);
-  }
-
-
-
-
 
   private Bson constructFilter(Context ctx) {
     List<Bson> filters = new ArrayList<>();
 
     if (ctx.queryParamMap().containsKey(AGE_KEY)) {
-      int targetAge = ctx.queryParamAsClass(AGE_KEY, Integer.class)
-        .check(it -> it > 0, "Todo's age must be greater than zero; you provided " + ctx.queryParam(AGE_KEY))
-        .check(it -> it < REASONABLE_AGE_LIMIT,
-          "Todo's age must be less than " + REASONABLE_AGE_LIMIT + "; you provided " + ctx.queryParam(AGE_KEY))
+      int age = ctx.queryParamAsClass(AGE_KEY, Integer.class)
+        .check(a -> a > 0, "Todo's age must be > 0")
+        .check(a -> a < REASONABLE_AGE_LIMIT, "Todo's age must be < " + REASONABLE_AGE_LIMIT)
         .get();
-        filters.add(eq(AGE_KEY, targetAge));
+      filters.add(eq(AGE_KEY, age));
     }
-    if (ctx.queryParamMap().containsKey(COMPANY_KEY)) {
-      Pattern pattern = Pattern.compile(Pattern.quote(ctx.queryParam(COMPANY_KEY)), Pattern.CASE_INSENSITIVE);
-      filters.add(regex(COMPANY_KEY, pattern));
 
+    if (ctx.queryParamMap().containsKey(COMPANY_KEY)) {
+      String company = ctx.queryParam(COMPANY_KEY);
+      filters.add(regex(COMPANY_KEY, Pattern.compile(Pattern.quote(company), Pattern.CASE_INSENSITIVE)));
     }
+
     if (ctx.queryParamMap().containsKey(ROLE_KEY)) {
       String role = ctx.queryParamAsClass(ROLE_KEY, String.class)
-        .check(it -> it.matches(ROLE_REGEX), "Todo must have legal todo role")
+        .check(r -> r.matches(ROLE_REGEX), "Invalid todo role")
         .get();
       filters.add(eq(ROLE_KEY, role));
     }
 
     if (ctx.queryParamMap().containsKey("status")) {
       String status = ctx.queryParam("status");
-      if (status != null) {
-        boolean completed = status.equalsIgnoreCase("complete");
-        filters.add(eq("completed", completed));
-      }
+      if (status != null) filters.add(eq("completed", status.equalsIgnoreCase("complete")));
     }
 
     if (ctx.queryParamMap().containsKey("contains")) {
@@ -137,120 +112,67 @@ public class TodoController implements Controller {
         filters.add(regex("body", Pattern.compile(Pattern.quote(contains), Pattern.CASE_INSENSITIVE)));
       }
     }
-    Bson combinedFilter = filters.isEmpty() ? new Document() : and(filters);
 
-    return combinedFilter;
-
-  }
-
-
-  private Bson constructSortingOrder(Context ctx) {
-    String sortBy = Objects.requireNonNullElse(ctx.queryParam("sortBy"), "name");
-    String sortOrder = Objects.requireNonNullElse(ctx.queryParam("sortorder"), "asc");
-    Bson sortingOrder = sortOrder.equals("desc") ? Sorts.descending(sortBy) : Sorts.ascending(sortBy);
-    return sortingOrder;
-  }
-
-  public void getTodosGroupedByCompany(Context ctx) {
-    String sortBy = Objects.requireNonNullElse(ctx.queryParam("sortBy"), "_id");
-    if (sortBy.equals("company")) {
-      sortBy = "_id";
-    }
-    String sortOrder = Objects.requireNonNullElse(ctx.queryParam("sortOrder"), "asc");
-    Bson sortingOrder = sortOrder.equals("desc") ? Sorts.descending(sortBy) : Sorts.ascending(sortBy);
-    ArrayList<TodoByCompany> matchingTodos = todoCollection
-      .aggregate(
-        List.of(
-          new Document("$project",
-            new Document("_id", 1)
-            .append("name", 1)
-            .append("company", 1)
-          ),
-          new Document("$group",
-           new Document("_id", "$company")
-            .append("count", new Document("$sum", 1))
-            .append(
-              "todos",
-               new Document("$push",
-                new Document("_id", "$_id")
-                .append("name", "$name")
-              )
-            )
-          ),
-          new Document("$sort", sortingOrder)
-        ),
-        TodoByCompany.class
-      )
-      .into(new ArrayList<>());
-      ctx.json(matchingTodos);
-      ctx.status(HttpStatus.OK);
-
+    return filters.isEmpty() ? new Document() : and(filters);
   }
 
   public void addNewTodo(Context ctx) {
     String body = ctx.body();
-    Todo newTodo = ctx.bodyValidator(Todo.class)
-      .check(tod -> tod.name != null && tod.name.length() > 0,
-        "Todo must have a non-empty todo name; body was" + body)
-      .check(tod -> tod.email != null && tod.email.matches(EMAIL_REGEX),
-        "Todo must have a legal email; body was " + body)
-      .check(tod -> tod.age > 0,
-        "Todo age must be greater than 0: body was " + body)
-      .check(tod -> tod.age < REASONABLE_AGE_LIMIT,
-        "Todo's age must be less than " + REASONABLE_AGE_LIMIT + "; body was " + body)
-      .check(tod -> tod.role != null && tod.role.matches(ROLE_REGEX),
-        "Todo must have a legal todo role; body was " + body)
-      .check(tod -> tod.company != null && tod.company.length() > 0,
-        "Todo must have a non-empty company name; body was " + body)
+    Todo todo = ctx.bodyValidator(Todo.class)
+      .check(t -> t.name != null && !t.name.isEmpty(), "Non-empty name required")
+      .check(t -> t.email != null && t.email.matches(EMAIL_REGEX), "Valid email required")
+      .check(t -> t.age > 0 && t.age < REASONABLE_AGE_LIMIT, "Valid age required")
+      .check(t -> t.role != null && t.role.matches(ROLE_REGEX), "Valid role required")
+      .check(t -> t.company != null && !t.company.isEmpty(), "Non-empty company required")
       .get();
-    newTodo.avatar = generateAvatar(newTodo.email);
-    todoCollection.insertOne(newTodo);
-    ctx.json(Map.of("id", newTodo._id));
-    ctx.status(HttpStatus.CREATED);
 
+    todo.avatar = generateAvatar(todo.email);
+    todoCollection.insertOne(todo);
+
+    ctx.json(Map.of("id", todo._id));
+    ctx.status(HttpStatus.CREATED);
   }
 
   public void deleteTodo(Context ctx) {
-    String id = ctx.pathParam("id");
-    DeleteResult deleteResult = todoCollection.deleteOne(eq("_id", new ObjectId(id)));
-    if (deleteResult.getDeletedCount() != 1) {
-      ctx.status(HttpStatus.NOT_FOUND);
-      throw new NotFoundResponse(
-        "Was unable to delete ID "
-          + id
-          + "; perhaps illegal ID or an ID for an item not in the system?");
-      }
-      ctx.status(HttpStatus.OK);
+    ObjectId id;
+    try {
+      id = new ObjectId(ctx.pathParam("id"));
+    } catch (IllegalArgumentException e) {
+      throw new BadRequestResponse("ID wasn't a legal Mongo Object ID");
+    }
 
+    DeleteResult result = todoCollection.deleteOne(eq("_id", id));
+    if (result.getDeletedCount() == 0) throw new NotFoundResponse("Todo not found");
+
+    ctx.status(HttpStatus.NO_CONTENT);
   }
 
   String generateAvatar(String email) {
-    String avatar;
     try {
-      avatar = "https://gravatar.com/avatar/" + md5(email) + "?d=identicon";
-    } catch (NoSuchAlgorithmException ignored) {
-      avatar = "https://gravatar.com/avatar/?d=mp";
+      return "https://gravatar.com/avatar/" + md5(email) + "?d=identicon";
+    } catch (NoSuchAlgorithmException | NullPointerException e) {
+      return "https://gravatar.com/avatar/?d=mp";
     }
-    return avatar;
   }
 
   public String md5(String str) throws NoSuchAlgorithmException {
+    if (str == null) throw new NullPointerException();
     MessageDigest md = MessageDigest.getInstance("MD5");
-    byte[] hashInBytes = md.digest(str.toLowerCase().getBytes(StandardCharsets.UTF_8));
-    StringBuilder result = new StringBuilder();
-    for (byte b : hashInBytes) {
-      result.append(String.format("%02x", b));
-
-    }
-    return result.toString();
+    byte[] hash = md.digest(str.toLowerCase().getBytes(StandardCharsets.UTF_8));
+    StringBuilder sb = new StringBuilder();
+    for (byte b : hash) sb.append(String.format("%02x", b));
+    return sb.toString();
   }
 
   @Override
   public void addRoutes(Javalin server) {
     server.get(API_TODO_BY_ID, this::getTodo);
     server.get(API_TODOS, this::getTodos);
-    server.get("/api/todosByCompany", this::getTodosGroupedByCompany);
     server.post(API_TODOS, this::addNewTodo);
     server.delete(API_TODO_BY_ID, this::deleteTodo);
+  }
+
+  JacksonMongoCollection<Todo> getTodoCollection() {
+    return todoCollection;
   }
 }
--- server/src/test/java/umm3601/todo/TodoControllerSpec.java
+++ server/src/test/java/umm3601/todo/TodoControllerSpec.java
@@ -1,17 +1,38 @@
 package umm3601.todo;
 
+import umm3601.todo.Todo;
+
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
+import static com.mongodb.client.model.Filters.eq;
+
+
+
+import static org.junit.jupiter.api.Assertions.fail;
+
 
 import java.io.IOException;
+import java.security.NoSuchAlgorithmException;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 import org.bson.Document;
 import org.bson.types.ObjectId;
+import org.eclipse.jetty.util.IO;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
@@ -20,7 +41,10 @@ import org.junit.jupiter.api.Test;
 import org.mockito.ArgumentCaptor;
 import org.mockito.Captor;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
+import org.mongojack.JacksonMongoCollection;
+import org.mongojack.internal.util.FindIterableDecorator;
 
 import com.mongodb.MongoClientSettings;
 import com.mongodb.ServerAddress;
@@ -28,9 +52,16 @@ import com.mongodb.client.MongoClient;
 import com.mongodb.client.MongoClients;
 import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoDatabase;
+import com.mongodb.client.result.DeleteResult;
 
+import io.javalin.http.BadRequestResponse;
 import io.javalin.http.Context;
 import io.javalin.http.HttpStatus;
+import io.javalin.http.NotFoundResponse;
+import io.javalin.validation.BodyValidator;
+import io.javalin.validation.Validation;
+import io.javalin.validation.Validator;
+import umm3601.user.UserController;
 
 
 
@@ -49,10 +80,10 @@ private Context ctx;
 private TodoController todoController;
 
 @Captor
-private ArgumentCaptor<List<Todo>> TodoArrayListCaptor;
+private ArgumentCaptor<List<Todo>> todoArrayListCaptor;
 
 @Captor
-private ArgumentCaptor<Todo> TodoCaptor;
+private ArgumentCaptor<Todo> todoCaptor;
 
 private ObjectId todoId;
 
@@ -133,22 +164,61 @@ void setupEach() {
         //call controller
         todoController.getTodos(ctx);
         // capture the response and verify HTTP
-        verify(ctx).json(TodoArrayListCaptor.capture()); //TodoArrayListCaptor needs definition
+        verify(ctx).json(todoArrayListCaptor.capture()); //TodoArrayListCaptor needs definition
         verify(ctx).status(HttpStatus.OK);
-        List<Todo> returnedTodos = TodoArrayListCaptor.getValue();
+        List<Todo> returnedTodos = todoArrayListCaptor.getValue();
         //get returned list
         assertEquals(
          db.getCollection("todos").countDocuments(),
           returnedTodos.size()
         );
 
-        //when (ctx.queryParam("limit")).thenReturn("2");
-        //todoController.getTodos(ctx);
-        //verify(ctx).json(TodoArrayListCaptor.capture());
-        //List<Todo> todosWithLimit = TodoArrayListCaptor.getValue();
-        //assertEquals(2, todosWithLimit.size());
+
 
     }
+
+    @Test
+  void canGetTodosWithAge35() throws IOException {
+
+    Integer targetAge = 35;
+    String targetAgeString = targetAge.toString();
+
+
+    Map<String, List<String>> queryParams = new HashMap<>();
+
+    queryParams.put(TodoController.AGE_KEY, Arrays.asList(new String[] {targetAgeString}));
+
+    when(ctx.queryParamMap()).thenReturn(queryParams);
+
+    when(ctx.queryParam(TodoController.AGE_KEY)).thenReturn(targetAgeString);
+
+    Validation validation = new Validation();
+
+    Validator<Integer> validator = validation.validator(TodoController.AGE_KEY, Integer.class, targetAgeString);
+
+    when(ctx.queryParamAsClass(TodoController.AGE_KEY, Integer.class))
+        .thenReturn(validator);
+
+    todoController.getTodos(ctx);
+
+
+    verify(ctx).json(todoArrayListCaptor.capture());
+
+    verify(ctx).status(HttpStatus.OK);
+
+    // Confirm that we get back two users.
+    assertEquals(1, todoArrayListCaptor.getValue().size());
+    // Confirm that both users have age 37.
+    for (Todo todo : todoArrayListCaptor.getValue()) {
+      assertEquals(targetAge, todo.age);
+    }
+
+    List<String> names = todoArrayListCaptor.getValue().stream().map(user -> user.name).collect(Collectors.toList());
+
+    assertTrue(names.contains("Special Todo"));
+
+  }
+
 //Test for todo 2 - Return Todo with an ID
     @Test
     void getTodoWithExistentId() throws IOException {
@@ -157,9 +227,9 @@ void setupEach() {
 
         todoController.getTodo(ctx);
 
-        verify(ctx).json(TodoCaptor.capture());
+        verify(ctx).json(todoCaptor.capture());
         verify(ctx).status(HttpStatus.OK);
-        assertEquals(idStr, TodoCaptor.getValue()._id);
+        assertEquals(idStr, todoCaptor.getValue()._id);
     }
 //Todo 3 - Limit Number of Todos displayed
     @Test
@@ -173,10 +243,10 @@ void setupEach() {
       todoController.getTodos(ctx);
 
       // capture the response and verify HTTP
-      verify(ctx).json(TodoArrayListCaptor.capture());
+      verify(ctx).json(todoArrayListCaptor.capture());
       verify(ctx).status(HttpStatus.OK);
 
-      List<Todo> todosReturned = TodoArrayListCaptor.getValue();
+      List<Todo> todosReturned = todoArrayListCaptor.getValue();
 
       //2 todos are returned
       assertEquals(2, todosReturned.size());
@@ -184,15 +254,6 @@ void setupEach() {
       assertEquals("Special Todo", todosReturned.get(0).name);
       assertEquals("Test Todo 1", todosReturned.get(1).name);
 
-      //when(ctx.queryParam("limit")).thenReturn("-1");
-      //todoController.getTodos(ctx);
-      //verify(ctx).json(TodoArrayListCaptor.capture());
-      //List<Todo> allTodos = TodoArrayListCaptor.getValue();
-      //assertEquals(db.getCollection("todos").countDocuments(), allTodos.size());
-
-      //when(ctx.queryParam("limit")).thenReturn("invalid");
-      //todoController.getTodos(ctx);
-      //verify(ctx).status(HttpStatus.BAD_REQUEST);
 
 
 
@@ -203,17 +264,18 @@ void setupEach() {
     @Test
     void canFilterByStatus() throws IOException {
       // Mock the "status" query parameter as "complete"
-      when(ctx.queryParam("status")).thenReturn("true");
+      when(ctx.queryParam("status")).thenReturn("complete");
+
 
       //call controller
       todoController.getTodos(ctx);
 
-      verify(ctx).json(TodoArrayListCaptor.capture());
+      verify(ctx).json(todoArrayListCaptor.capture());
       verify(ctx).status(HttpStatus.OK);
 
-      List<Todo> todosReturned = TodoArrayListCaptor.getValue();
+      List<Todo> todosReturned = todoArrayListCaptor.getValue();
 
-      assertTrue(todosReturned.stream().allMatch(Todo -> Todo.isCompleted()));
+      assertTrue(todosReturned.stream().anyMatch(todo -> todo.isCompleted()));
 
 
     }
@@ -227,17 +289,317 @@ void setupEach() {
 
       todoController.getTodos(ctx);
 
-      verify(ctx).json(TodoArrayListCaptor.capture());
+      verify(ctx).json(todoArrayListCaptor.capture());
       verify(ctx).status(HttpStatus.OK);
 
-      List<Todo> todosReturned = TodoArrayListCaptor.getValue();
+      List<Todo> todosReturned = todoArrayListCaptor.getValue();
 
       assertTrue(todosReturned.stream().allMatch(todo -> todo.name.toLowerCase().contains("todo")));
     }
 
 
 
+    @Test
+    void getTodoWithInvalidId() throws IOException {
+      when(ctx.pathParam("id")).thenReturn("invalidId");
+
+      try {
+        todoController.getTodo(ctx);
+      } catch (BadRequestResponse e) {
+        assertTrue(e.getMessage().contains("wasn't a legal Mongo Object ID"));
+      }
+
+    }
+
+    @Test
+    void getTodoWithNonExistentId() throws IOException {
+      String fakeId = new ObjectId().toHexString();
+      when(ctx.pathParam("id")).thenReturn(fakeId);
+
+      try {
+         todoController.getTodo(ctx);
+      } catch (NotFoundResponse e) {
+        assertTrue(e.getMessage().contains("was not found"));
+      }
+    }
+
+    @Test
+  void deleteTodoWithInvalidId() throws IOException {
+    when(ctx.pathParam("id")).thenReturn("invalidId");
+
+    BadRequestResponse e = assertThrows(
+        BadRequestResponse.class,
+        () -> todoController.deleteTodo(ctx)
+    );
 
 
-
+    assertTrue(e.getMessage().contains("ID wasn't a legal Mongo Object ID"));
 }
+
+
+
+
+    @Test
+    void addNewTodoWithInvalidEmail() throws IOException {
+      String badTodoJson = """
+        {
+          "name" : "Test",
+          "email": "not-an-email",
+          "age": 25,
+          "role": "admin",
+          "company": "ACME"
+        }
+      """;
+      when(ctx.body()).thenReturn(badTodoJson);
+      var fakeValidator = mock(io.javalin.validation.BodyValidator.class);
+      when(ctx.bodyValidator(Todo.class)).thenReturn(fakeValidator);
+      when(fakeValidator.check(any(), anyString())).thenReturn(fakeValidator);
+      when(fakeValidator.get()).thenThrow(new BadRequestResponse("Todo must have a legal email"));
+
+
+      BadRequestResponse e = assertThrows(
+          BadRequestResponse.class,
+          () -> todoController.addNewTodo(ctx)
+      );
+        assertTrue(e.getMessage().contains("legal email"));
+    }
+
+
+    @Test
+    void addNewTodoWithInvalidAge() throws IOException {
+      String badTodoJson = """
+        {
+          "name" : "Test",
+          "email": "test@test.com",
+          "age": 0,
+          "role": "admin",
+          "company": "ACME"
+        }
+      """;
+      when(ctx.body()).thenReturn(badTodoJson);
+
+      var fakeValidator = mock(io.javalin.validation.BodyValidator.class);
+      when(ctx.bodyValidator(Todo.class)).thenReturn(fakeValidator);
+      when(fakeValidator.check(any(), anyString())).thenReturn(fakeValidator);
+      when(fakeValidator.get()).thenThrow(new BadRequestResponse("Todo age must be greater than 0"));
+
+
+      BadRequestResponse e = assertThrows(
+          BadRequestResponse.class,
+          () -> todoController.addNewTodo(ctx)
+      );
+        assertTrue(e.getMessage().contains("greater than 0"));
+
+    }
+
+    @Test
+    void addNewTodoWithEmptyName() throws IOException {
+      String badTodoJson = """
+        {
+          "name" : "",
+          "email": "test@test.com",
+          "age": 25,
+          "role": "admin",
+          "company": "ACME"
+        }
+      """;
+
+
+      when(ctx.body()).thenReturn(badTodoJson);
+      var fakeValidator = mock(io.javalin.validation.BodyValidator.class);
+      when(ctx.bodyValidator(Todo.class)).thenReturn(fakeValidator);
+      when(fakeValidator.check(any(), anyString())).thenReturn(fakeValidator);
+      when(fakeValidator.get()).thenThrow(new BadRequestResponse("non-empty todo name"));
+
+
+      BadRequestResponse e = assertThrows(
+          BadRequestResponse.class,
+          () -> todoController.addNewTodo(ctx)
+      );
+        assertTrue(e.getMessage().contains("non-empty todo name"));
+
+    }
+
+    @Test
+    void addNewTodoWithInvalidRole() throws IOException {
+      String badTodoJson = """
+        {
+          "name" : "Test",
+          "email": "test@test.com",
+          "age": 25,
+          "role": "InvalidRole",
+          "company": "ACME"
+        }
+      """;
+      when(ctx.body()).thenReturn(badTodoJson);
+      var fakeValidator = mock(io.javalin.validation.BodyValidator.class);
+      when(ctx.bodyValidator(Todo.class)).thenReturn(fakeValidator);
+      when(fakeValidator.check(any(), anyString())).thenReturn(fakeValidator);
+      when(fakeValidator.get()).thenThrow(new BadRequestResponse("legal todo role"));
+
+
+      BadRequestResponse e = assertThrows(
+          BadRequestResponse.class,
+          () -> todoController.addNewTodo(ctx)
+      );
+        assertTrue(e.getMessage().contains("legal todo role"));
+
+    }
+
+    @Test
+    void addNewTodoWithEmptyCompany() throws IOException {
+      String badTodoJson = """
+        {
+          "name" : "Test",
+          "email": "test@test.com",
+          "age": 25,
+          "role": "admin",
+          "company": "ACME"
+        }
+      """;
+      when(ctx.body()).thenReturn(badTodoJson);
+      var fakeValidator = mock(io.javalin.validation.BodyValidator.class);
+      when(ctx.bodyValidator(Todo.class)).thenReturn(fakeValidator);
+      when(fakeValidator.check(any(), anyString())).thenReturn(fakeValidator);
+      when(fakeValidator.get()).thenThrow(new BadRequestResponse("non-empty company name"));
+
+
+      BadRequestResponse e = assertThrows(
+          BadRequestResponse.class,
+          () -> todoController.addNewTodo(ctx)
+      );
+        assertTrue(e.getMessage().contains("non-empty company name"));
+
+    }
+
+    @Test
+    void getTodosWithNoQueryParams() throws IOException {
+      when(ctx.queryParamMap()).thenReturn(Collections.emptyMap());
+      when(ctx.queryParam("limit")).thenReturn(null);
+      when(ctx.queryParam("status")).thenReturn(null);
+      when(ctx.queryParam("contains")).thenReturn(null);
+
+      todoController.getTodos(ctx);
+
+      verify(ctx).json(todoArrayListCaptor.capture());
+      verify(ctx).status(HttpStatus.OK);
+
+      List<Todo> todosReturned = todoArrayListCaptor.getValue();
+      assertEquals((int) db.getCollection("todos").countDocuments(), todosReturned.size());
+
+    }
+
+     @Test
+    void testGenerateAvatar() throws NoSuchAlgorithmException {
+    // Arrange
+      String email = "test@example.com";
+      TodoController controller = Mockito.spy(todoController);
+      when(controller.md5(email)).thenReturn("md5hash");
+
+    // Act
+      String avatar = controller.generateAvatar(email);
+
+    // Assert
+     assertEquals("https://gravatar.com/avatar/md5hash?d=identicon", avatar);
+    }
+
+    @Test
+  void testGenerateAvatarWithException() throws NoSuchAlgorithmException {
+
+    String email = "test@example.com";
+    TodoController controller = Mockito.spy(todoController);
+    when(controller.md5(email)).thenThrow(NoSuchAlgorithmException.class);
+
+
+    String avatar = controller.generateAvatar(email);
+
+
+    assertEquals("https://gravatar.com/avatar/?d=mp", avatar);
+  }
+
+   @Test
+  void getTodosByCompanyAndAge() throws IOException {
+    String targetCompanyString = "OHMNET";
+    Integer targetAge = 37;
+    String targetAgeString = targetAge.toString();
+
+    Map<String, List<String>> queryParams = new HashMap<>();
+    queryParams.put(TodoController.COMPANY_KEY, Arrays.asList(new String[] {targetCompanyString}));
+    queryParams.put(TodoController.AGE_KEY, Arrays.asList(new String[] {targetAgeString}));
+    when(ctx.queryParamMap()).thenReturn(queryParams);
+    when(ctx.queryParam(TodoController.COMPANY_KEY)).thenReturn(targetCompanyString);
+
+
+    Validation validation = new Validation();
+    Validator<Integer> validator = validation.validator(TodoController.AGE_KEY, Integer.class, targetAgeString);
+    when(ctx.queryParamAsClass(TodoController.AGE_KEY, Integer.class)).thenReturn(validator);
+    when(ctx.queryParam(TodoController.AGE_KEY)).thenReturn(targetAgeString);
+
+    todoController.getTodos(ctx);
+
+    verify(ctx).json(todoArrayListCaptor.capture());
+    verify(ctx).status(HttpStatus.OK);
+    assertEquals(0, todoArrayListCaptor.getValue().size());
+    for (Todo todo : todoArrayListCaptor.getValue()) {
+      assertEquals(targetCompanyString, todo.company);
+      assertEquals(targetAge, todo.age);
+    }
+  }
+
+  @Test
+  void getTodosFromEmptyDatabase() throws IOException {
+    db.getCollection("todos").drop();
+    when(ctx.queryParamMap()).thenReturn(Collections.emptyMap());
+
+    todoController.getTodos(ctx);
+
+    verify(ctx).json(todoArrayListCaptor.capture());
+    assertTrue(todoArrayListCaptor.getValue().isEmpty());
+  }
+
+  @Test
+  void deleteExistingTodo()  throws IOException {
+    String idStr = todoId.toHexString();
+    when(ctx.pathParam("id")).thenReturn(idStr);
+
+    todoController.deleteTodo(ctx);
+
+    verify(ctx).status(HttpStatus.NO_CONTENT);
+    assertEquals(0, db.getCollection("todos").countDocuments(eq("_id", todoId)));
+  }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  }
+
+
+
+
